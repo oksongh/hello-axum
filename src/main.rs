68ea76ef871955c8c78412718243446946fa987a
oksongh <oksongh@gmail.com>
2024-10-14 19:21:56 +0900

    include html file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 // Copyright (c) 2021 Axum Tutorial Contributors
 // SPDX-License-Identifier: MIT
 // https://github.com/programatik29/axum-tutorial/
-use axum::{extract::Query, response::Html, routing::get, Router};
-use rand::{thread_rng, Rng};
-use serde::Deserialize;
+use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
@@ -13,13 +11,7 @@ async fn main() {
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
-#[derive(Deserialize)]
-struct RangeParams {
-    start: usize,
-    end: usize,
-}
-async fn handler(Query(range): Query<RangeParams>) -> Html<String> {
-    let random_num = thread_rng().gen_range(range.start..range.end);
-    println!("hey:{random_num}");
-    Html(format!("<h1>Random number: {random_num}</h1>"))
+
+async fn handler() -> Html<&'static str> {
+    Html(include_str!("../index.html"))
 }
